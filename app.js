bd8cae1dfe70e1b7b3a5dc72eed2f6ef0d53288f
andhika16 <moh.andhika16@gmail.com>
2020-08-29 10:57:14 +0700

    initial commit

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 const express = require('express');
 const app = express();
+const PORT = 4000;
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
+
+app.listen(PORT, () => {
+    console.log(`server connected ${PORT}`);
+})
